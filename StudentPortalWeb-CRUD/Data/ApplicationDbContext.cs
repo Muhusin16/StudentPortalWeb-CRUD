6b4f3df6a8498f338ecf6add8e880056e946f202
muhus <muhus@DESKTOP-V1LLKIG>
2025-04-24 17:26:02 +0530

    Add project files.

---
+++ StudentPortalWeb-CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortalWeb_CRUD.Models.Entities;
+
+namespace StudentPortalWeb_CRUD.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentPortalWeb-CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentPortalWeb_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
